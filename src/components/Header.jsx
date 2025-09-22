a9346ef02030d2c317d9d1a2dc9347480c55a099
Rifah Arif <arifalilko2018@gmail.com>
2025-09-22 20:47:39 +0530

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/Header.css";
 import { FaHome, FaBell, FaEnvelope, FaUser } from "react-icons/fa";
+import Logo from "../assets/logo.png"; 
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="logo">Vibe</div>
+      <div className="logo">
+        <img src={Logo} alt="Vibe Logo" className="logo-img" />
+      </div>
       <input className="search" placeholder="Search..." />
       <nav className="nav">
         <button>
