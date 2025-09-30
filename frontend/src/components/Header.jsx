ca6afad2b21099b7cb976605d695f967b3f0d90b
Rifah27 <arifalilko2018@gmail.com>
2025-09-30 23:58:54 +0530

    new chnges

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/Header.css";
 import { FaHome, FaBell, FaEnvelope, FaUser, FaSearch } from "react-icons/fa";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logoo.jpg";
 
 export default function Header() {
   const [searchQuery, setSearchQuery] = useState("");
