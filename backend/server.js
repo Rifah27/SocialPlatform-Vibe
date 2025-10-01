ced4ce29fa82e6a8f424a8912e33836aba69fa9a
Rifah27 <arifalilko2018@gmail.com>
2025-10-01 15:22:12 +0530

    chnges

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect("mongodb://127.0.0.1:27017/social-media", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
