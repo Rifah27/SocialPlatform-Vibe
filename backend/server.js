7ffffed3f7c754a794923e9226125f44b4113eca
Rifah27 <arifalilko2018@gmail.com>
2025-10-02 19:10:39 +0530

    chnges

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/social-media", {
+mongoose.connect("mongodb+srv://aliaaa_s:Incorrect%401@cluster0.h2mcxgd.mongodb.net/social-media?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
